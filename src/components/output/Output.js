8c0e7531f440bdf857d8c493daf80e199df2606e
TranHBach <ragoza0932447933@gmail.com>
2022-03-16 09:07:03 +0700

    Pass the fourth criteria

--- src/components/output/Output.js
+++ src/components/output/Output.js
@@ -1,15 +1,17 @@
 import { useContext } from "react";
 import Context from "../store/Context";
-import './Output.module.css'
+import "./Output.module.css";
 import { marked } from "marked";
 const Output = () => {
   const contextValue = useContext(Context);
   const HTMLText = marked.parse(contextValue.value);
   //   const result = HTMLText.replace(/\n/g, <br/>)
   return (
-    <div className="col-6 border" id="preview">
-      <div dangerouslySetInnerHTML={{ __html: HTMLText }} />
-    </div>
+    <div
+      className="col-6 border"
+      id="preview"
+      dangerouslySetInnerHTML={{ __html: HTMLText }}
+    />
   );
 };
 
